4b55e04d7558256235be7cc1616a3c976a4ac6b9
dongdong <gangsta98t@gmail.com>
2023-09-09 14:32:13 +0800

    finished HW6. except TEST7.

--- HW6/include/q4.h
+++ HW6/include/q4.h
@@ -1,4 +1,31 @@
 #ifndef Q4_H
 #define Q4_H
 
+#include <vector>
+
+namespace q4 {
+
+    struct Vector2D {
+        double x{};
+        double y{};
+    };
+
+    struct Sensor {
+        Vector2D pos;
+        double accuracy;
+    };
+
+    inline Vector2D kalman_filter(const std::vector<Sensor>& sensors) {
+        double acc = 0;
+        double x = 0, y = 0;
+        for (const auto &item: sensors) {
+            acc += item.accuracy;
+            x += item.pos.x * item.accuracy;
+            y += item.pos.y * item.accuracy;
+        }
+        return Vector2D(x / acc, y / acc);
+    }
+
+}
+
 #endif //Q4_H
